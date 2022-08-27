fa7660483195242562f28a3c98c1846ffa8f7033
Masahiro Yamauchi <oldblend@hotmail.com>
2022-08-27 12:01:36 +0000

    update dependencies

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,14 @@
-import { server } from './server';
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { schema } from './schema';
 
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`)
+const port = 4000;
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+}));
+app.listen(port, () => {
+  console.log(`🚀 Server ready at http://localhost:${port}/graphql`);
 });
