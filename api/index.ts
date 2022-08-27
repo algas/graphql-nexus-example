072c7701b6b245587ae432a2ff0898dd5c90cc6f
Masahiro Yamauchi <oldblend@hotmail.com>
2022-08-27 13:30:03 +0000

    refactor

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,6 @@
-import express from 'express';
-import { graphqlHTTP } from 'express-graphql';
-import { schema } from './schema';
-import { context } from './context';
-
+import { app } from './server';
 const port = 4000;
-const app = express();
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  context,
-  graphiql: true,
-}));
 app.listen(port, () => {
   console.log(`🚀 Server ready at http://localhost:${port}/graphql`);
 });
