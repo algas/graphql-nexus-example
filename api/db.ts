22be4db27fe72e59ce3a67b3ba30acad7745a47c
Masahiro Yamauchi <oldblend@hotmail.com>
2022-08-27 12:39:02 +0000

    context

---
+++ api/db.ts
@@ -0,0 +1,11 @@
+import { NexusGenFieldTypes } from '../nexus-typegen';
+
+export type Post = NexusGenFieldTypes['Post'];
+
+export interface Db {
+  posts: Post[];
+};
+
+export const db: Db = {
+  posts: [{ id: 1, title: 'Nexus', body: '...', published: false }],
+};
--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { schema } from './schema';
+import { context } from './context';
 
 const port = 4000;
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
   schema,
+  context,
   graphiql: true,
 }));
 app.listen(port, () => {
